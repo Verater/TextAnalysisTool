93cb694e9d4c9cd92926dbe6d6e43d67cbb9389f
Verater <126973356+Verater@users.noreply.github.com>
2023-11-04 06:11:16 +0000

    Remove temp file

---
+++ services/web/tests/test_transform_lib.py
@@ -0,0 +1,8 @@
+from analyzer.constraint_lib import (
+    ExactMatch, MatchAny, DoesNotMatch, DoesNotMatchAny, HasText, DoesNotHaveText,
+    Parameter, TransformList,
+)
+
+import logging
+
+logging.basicConfig(level
